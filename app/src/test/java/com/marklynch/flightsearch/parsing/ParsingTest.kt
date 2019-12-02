7b6bb088b60322037f5071b98116de96a4c10591
Mark Lynch <lynchm3@tcd.ie>
2019-12-02 11:35:13 +0000

    Tests for dates implied to be next year

--- app/src/test/java/com/marklynch/flightsearch/parsing/ParsingTest.kt
+++ app/src/test/java/com/marklynch/flightsearch/parsing/ParsingTest.kt
@@ -8,7 +8,6 @@ class ParsingTest {
     @Test
     fun `Test date parsing`() {
 
-//        val currentDate:Date = Date()
         val currentDateCalendar:Calendar = Calendar.getInstance()
 
         val searchTerm = """Manchester to Barcelona
@@ -31,25 +30,29 @@ class ParsingTest {
         assertEquals("Month for second date incorrect", Calendar.DECEMBER, calendar.get(Calendar.MONTH))
         assertEquals("Year for second date incorrect", currentDateCalendar.get(Calendar.YEAR), calendar.get(Calendar.YEAR))
     }
+    @Test
+    fun `Test date parsing leaving and returning next year`() {
 
-//    @Test
-//    fun `Test date parsing return next year`() {
-//        val searchTerm = """Manchester to Barcelona
-//         for one adult
-//         returning on the 19th of October
-//         going out on the 1st of January"""
-//
-//        val parsedDates = parseDatesAndSort(searchTerm)
-//        val calendar = Calendar.getInstance()
-//
-//        assertEquals("Incorrect amount of dates parsed", 2, parsedDates.size)
-//
-//        calendar.time = parsedDates[0]
-//        assertEquals("Day of month for first date incorrect", 14, calendar.get(Calendar.DAY_OF_MONTH))
-//        assertEquals("Month for first date incorrect", Calendar.OCTOBER, calendar.get(Calendar.MONTH))
-//
-//        calendar.time = parsedDates[1]
-//        assertEquals("Day of month for second date incorrect", 19, calendar.get(Calendar.DAY_OF_MONTH))
-//        assertEquals("Month for second date incorrect", Calendar.OCTOBER, calendar.get(Calendar.MONTH))
-//    }
+        val currentDateCalendar:Calendar = Calendar.getInstance()
+
+        val searchTerm = """Manchester to Barcelona
+         for one adult
+         leaving January 1st
+         coming back January 2nd"""
+
+        val parsedDates = parseDatesAndSort(searchTerm)
+        val calendar = Calendar.getInstance()
+
+        assertEquals("Incorrect amount of dates parsed", 2, parsedDates.size)
+
+        calendar.time = parsedDates[0]
+        assertEquals("Day of month for first date incorrect", 1, calendar.get(Calendar.DAY_OF_MONTH))
+        assertEquals("Month for first date incorrect", Calendar.JANUARY, calendar.get(Calendar.MONTH))
+        assertEquals("Year for first date incorrect", currentDateCalendar.get(Calendar.YEAR) +1, calendar.get(Calendar.YEAR))
+
+        calendar.time = parsedDates[1]
+        assertEquals("Day of month for second date incorrect", 2, calendar.get(Calendar.DAY_OF_MONTH))
+        assertEquals("Month for second date incorrect", Calendar.JANUARY, calendar.get(Calendar.MONTH))
+        assertEquals("Year for second date incorrect", currentDateCalendar.get(Calendar.YEAR) +1, calendar.get(Calendar.YEAR))
+    }
 }
